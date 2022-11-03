a9afe3ce2e9743e58e9f9099616c4ebda258938a
SashaRusyn <sasharusyn11@gmail.com>
2022-11-04 00:20:44 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Contact from './Contact';
+import Home from './Home';
+import Registration from './Registration';
+import Sign from './Sign';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Sign" element={<Sign />}></Route>
+          <Route path="/Registration" element={<Registration />}></Route>
+          <Route path="/Contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Registration.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/Sign-Page-Styles.css';
+import Header from './Header';
+import Sign_Container from './Sign_Container';
+import Registration_Container from './Registration_Container';
+
+const Registration = () => {
+    return (
+        <div>
+            <Header />
+            <Registration_Container />
+        </div>
+    )
+}
+
+export default Registration;
